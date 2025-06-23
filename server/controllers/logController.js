62ed7bf4f0a7272c06d303af1492f9256f5b0538
GauravMethwani <137370388+GauravMethwani@users.noreply.github.com>
2025-06-23 18:23:58 +0530

    Update logController.js

--- server/controllers/logController.js
+++ server/controllers/logController.js
@@ -2,10 +2,33 @@ const ImportLog = require('../models/logs');
 
 const getImportLogs = async (req, res) => {
   try {
-    const logs = await ImportLog.find().sort({ timestamp: -1 });
-    res.status(200).json(logs);
+    
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+
+   
+    const logs = await ImportLog.find()
+      .sort({ timestamp: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    const totalLogs = await ImportLog.countDocuments();
+
+    res.status(200).json({
+      success: true,
+      page,
+      totalPages: Math.ceil(totalLogs / limit),
+      totalLogs,
+      logs,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch logs", error: error.message });
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch logs",
+      error: error.message,
+    });
   }
 };
 
